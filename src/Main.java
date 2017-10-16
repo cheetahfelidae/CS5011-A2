249007398dfba1d4bd9fb7f0002c7be26028bb9e
cheetahfelidae <@Spire5023nwlci>
2017-10-16 13:10:30 +0100

    Interim

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
-    char[][] map1 = new char[][]{
+    private static char[][] map1 = new char[][]{
             {'I', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'},
             {'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'},
             {'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'},
@@ -12,7 +15,7 @@ public class Main {
             {'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'G'},
     };
 
-    char[][] map2 = new char[][]{
+    private static char[][] map2 = new char[][]{
             {'O', 'O', 'X', 'O', 'O', 'O', 'O', 'O', 'O', 'X'},
             {'O', 'X', 'O', 'X', 'O', 'O', 'B', 'O', 'O', 'X'},
             {'O', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'O', 'O'},
@@ -25,7 +28,7 @@ public class Main {
             {'G', 'O', 'I', 'O', 'O', 'O', 'O', 'O', 'O', 'O'},
     };
 
-    char[][] map3 = new char[][]{
+    private static char[][] map3 = new char[][]{
             {'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'},
             {'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'},
             {'O', 'O', 'O', 'O', 'O', 'O', 'X', 'O', 'O', 'O'},
@@ -38,7 +41,7 @@ public class Main {
             {'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'}
     };
 
-    char[][] map4 = new char[][]{
+    private static char[][] map4 = new char[][]{
             {'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'X'},
             {'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'X', 'O'},
             {'O', 'O', 'O', 'O', 'O', 'O', 'X', 'X', 'O', 'O'},
@@ -51,7 +54,7 @@ public class Main {
             {'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'}
     };
 
-    char[][] map5 = new char[][]{
+    private static char[][] map5 = new char[][]{
             {'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'},
             {'O', 'O', 'B', 'O', 'O', 'O', 'O', 'O', 'O', 'O'},
             {'O', 'O', 'O', 'O', 'O', 'O', 'X', 'O', 'O', 'O'},
@@ -64,7 +67,7 @@ public class Main {
             {'X', 'O', 'O', 'O', 'O', 'O', 'O', 'X', 'O', 'O'}
     };
 
-    char[][] map6 = new char[][]{
+    private static char[][] map6 = new char[][]{
             {'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'X'},
             {'O', 'O', 'B', 'O', 'O', 'X', 'X', 'O', 'O', 'O'},
             {'O', 'X', 'X', 'X', 'O', 'O', 'X', 'O', 'O', 'I'},
@@ -79,6 +82,43 @@ public class Main {
 
 
     public static void main(String[] args) {
+//        ArrayList<int[][]> vertices = new ArrayList<>();
+//        int matrix_high = map1.length, matrix_wide = map1[0].length;
+//        for(int i = 0; i < matrix_high; i++) {
+//            int[][] neighbors = new int[matrix_high * matrix_wide][2];
+//
+//            for(int j = 0; j < matrix_high * matrix_wide; j++) {
+//                neighbors[j][0] = j / matrix_high;
+//            }
+//            for(int j = 0; j < matrix_high * matrix_wide; j++) {
+//                neighbors[j][1] = j % matrix_wide;
+//            }
+//            vertices.add(neighbors);
+//
+//            for (int[] n : neighbors) {
+//                System.out.print(Arrays.toString(n) + " ");
+//            }
+//            System.out.println();
+//            System.out.println();
+//        }
 
+        int[][] adj
+        for(int i = 0; i < matrix_high; i++) {
+            int[][] neighbors = new int[matrix_high * matrix_wide][2];
+
+            for(int j = 0; j < matrix_high * matrix_wide; j++) {
+                neighbors[j][0] = j / matrix_high;
+            }
+            for(int j = 0; j < matrix_high * matrix_wide; j++) {
+                neighbors[j][1] = j % matrix_wide;
+            }
+            vertices.add(neighbors);
+
+            for (int[] n : neighbors) {
+                System.out.print(Arrays.toString(n) + " ");
+            }
+            System.out.println();
+            System.out.println();
+        }
     }
 }
